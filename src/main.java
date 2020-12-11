ad7d08652e9be653a63b4694db78d50aafae37c3
Boris Jarczuk <yarchuk.b@gmail.com>
2020-12-12 02:11:30 +0300

    Money Transfer

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int current_balance = 2_000_000_000;
+        int income_amount = 500_000_000;
+        int sum = current_balance + income_amount;
+        System.out.println(sum);
+    }
+}
